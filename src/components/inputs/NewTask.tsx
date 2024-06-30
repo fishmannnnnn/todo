018212cb3e8e5198b999a32de058c67974ade914
Vlad <vlad.lemkov@yandex.ru>
2024-06-30 15:23:47 +0400

    chores: add some changes

---
+++ src/components/inputs/NewTask.tsx
@@ -0,0 +1,29 @@
+import styles from './NewTask.module.scss';
+import { useState } from "react";
+
+interface Props{
+	addTask: Function
+}
+
+const NewTask = ({addTask}: Props) => {
+	const [task, setTask] = useState('');
+	const handleClick = () => {
+		if(task!=='' && task!==undefined){
+			addTask(task)
+		}
+		setTask('');
+	}
+	return (
+		<div className={styles.newTask}>
+			<input
+				type='text'
+				placeholder='What do you need to do?'
+				value={task}
+				onChange={(e) => setTask(e.target.value)}
+			/>
+			<button className={styles.add} onClick={handleClick}>ADD</button>
+		</div>
+	);
+};
+
+export default NewTask;
--- src/components/task/TaskItem.tsx
+++ src/components/task/TaskItem.tsx
@@ -4,20 +4,24 @@ import { Task } from '../../App';
 interface Props {
 	task: Task;
 	updateTask: Function;
+	deleteTask: Function;
 }
 
-const TaskItem = (props: Props) => {
+const TaskItem = ({ task, updateTask, deleteTask }: Props) => {
 	return (
 		<div className={styles.task}>
 			<div className={styles.taskContent}>
 				<div
 					className={styles.nameWrapper}
 					onClick={() => {
-						props.task.isDone = !props.task.isDone;
-						props.updateTask(props.task);
+						task.isDone = !task.isDone;
+						updateTask(task);
 					}}>
-					{props.task.isDone ? (
-						<div className={styles.checkboxDone}>
+					<div
+						className={
+							task.isDone ? styles.checkboxDone : styles.checkbox
+						}>
+						{task.isDone ? (
 							<svg
 								width='46'
 								height='46'
@@ -29,37 +33,37 @@ const TaskItem = (props: Props) => {
 									fill='#D98326'
 								/>
 							</svg>
-						</div>
-					) : (
-						<div className={styles.checkbox}></div>
-					)}
+						) : (
+							''
+						)}
+					</div>
 					<div
 						className={
-							props.task.isDone
-								? styles.nameCompleted
-								: styles.name
+							task.isDone ? styles.nameCompleted : styles.name
 						}>
-						{props.task.name}
+						{task.name}
 					</div>
 				</div>
-				<svg
-					width='32'
-					height='32'
-					viewBox='0 0 32 32'
-					fill='none'
-					xmlns='http://www.w3.org/2000/svg'>
-					<g opacity='0.3' clipPath='url(#clip0_113_203)'>
-						<path
-							d='M7.99999 25.3333C7.99999 26.8 9.19999 28 10.6667 28H21.3333C22.8 28 24 26.8 24 25.3333V9.33333H7.99999V25.3333ZM10.6667 12H21.3333V25.3333H10.6667V12ZM20.6667 5.33333L19.3333 4H12.6667L11.3333 5.33333H6.66666V8H25.3333V5.33333H20.6667Z'
-							fill='#B30B04'
-						/>
-					</g>
-					<defs>
-						<clipPath id='clip0_113_203'>
-							<rect width='32' height='32' fill='white' />
-						</clipPath>
-					</defs>
-				</svg>
+				<button onClick={deleteTask(task.id)}>
+					<svg
+						width='32'
+						height='32'
+						viewBox='0 0 32 32'
+						fill='none'
+						xmlns='http://www.w3.org/2000/svg'>
+						<g opacity='0.3' clipPath='url(#clip0_113_203)'>
+							<path
+								d='M7.99999 25.3333C7.99999 26.8 9.19999 28 10.6667 28H21.3333C22.8 28 24 26.8 24 25.3333V9.33333H7.99999V25.3333ZM10.6667 12H21.3333V25.3333H10.6667V12ZM20.6667 5.33333L19.3333 4H12.6667L11.3333 5.33333H6.66666V8H25.3333V5.33333H20.6667Z'
+								fill='#B30B04'
+							/>
+						</g>
+						<defs>
+							<clipPath id='clip0_113_203'>
+								<rect width='32' height='32' fill='white' />
+							</clipPath>
+						</defs>
+					</svg>
+				</button>
 			</div>
 			<div className={styles.separator}></div>
 		</div>
