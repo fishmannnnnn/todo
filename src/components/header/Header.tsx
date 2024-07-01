45b637ec4bdcd2db6e2802421df38fe9f275606a
Vlad <vlad.lemkov@yandex.ru>
2024-07-01 12:09:51 +0400

    fix: fix img loading

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,7 +2,7 @@ import styles from './Header.module.scss'
 const Header = () => {
   return (
     <div className={styles.header}>
-        <img className={styles.header__logo} src="/src/assets/logo.png"/>
+        <img className={styles.header__logo} src="/logo.png"/>
     </div>
   )
 }
